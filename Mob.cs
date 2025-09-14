17ebd9af141fb4f65633c15f9b6a060fe75c0ea0
William Lim <william.lim2@telus.com>
2025-09-14 13:18:55 -0700

    complete mob scene

--- Mob.cs
+++ Mob.cs
@@ -9,4 +9,10 @@ public partial class Mob : RigidBody2D
         string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
         animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
     }
+
+    // We also specified this function name in PascalCase in the editor's connection window.
+    private void OnVisibleOnScreenNotifier2DScreenExited()
+    {
+        QueueFree();
+    }
 }
